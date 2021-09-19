6f1ce0cd84a05037d915de9554cd946ecb4f6b7c
cihanvol <63202692+cihanvol@users.noreply.github.com>
2021-09-19 17:09:15 +0300

    Update chat_config.py

--- plugins/chat_config.py
+++ plugins/chat_config.py
@@ -32,7 +32,7 @@ async def chdelay(client: Bot, message: Message):
     if " " in message.text:
         setdelay = message.text.split(" ")
         try:
-            Config.DELAY_SECS = int(setdelay[1])
+            Config.DELAY_SECS = float(setdelay[1])
             send = await message.reply_text(
                 quote=True,text=f"Delay set to: <code>{str(Config.DELAY_SECS)}</code> succesfully.")
         except:
@@ -41,7 +41,8 @@ async def chdelay(client: Bot, message: Message):
     else:
         send = await message.reply_text(quote=True,text=f"Delay is <code>{str(Config.DELAY_SECS)}</code> at now.\n" + \
         f"If you want to change it 7 secs, type <code>/{Config.CHANGE_DELAY_COMMAND} 7</code>\n" +\
-        f"7 is an example :D You can change delay to what you want.\n\n" + \
+        f"7 is an example :D You can change delay to what you want.\n" + \
+        f"If you want to disable delay, set it to <code>0.2</code>\n" + \
         f"If you want to see delay at now, type only <code>/{Config.CHANGE_DELAY_COMMAND}</code>")
     await asyncio.sleep(10)
     await send.delete()
